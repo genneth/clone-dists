95497f1a8aa17e30ec2ce0f27aad5d89acd43691
Gen Zhang <genneth@buck.(none)>
2009-10-22 20:58:07 +0100

    ask for higher tolerances

--- ABC/population.m
+++ ABC/population.m
@@ -2,6 +2,6 @@ function P = population(Tl, Tr, Tg, lambda, r, gamma, t, P0)
 
 % dP/dt = T P ==> P = P0 * exp(T t)
 
-P = expv(t, lambda*(Tl + r * Tr) + gamma*Tg, P0);
+P = expv(t, lambda*(Tl + r * Tr) + gamma*Tg, P0, 1e-10);
 
 end
