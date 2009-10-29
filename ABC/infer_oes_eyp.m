a38b2b6cd388ed6078faf0ff65e932f1db0e89a8
Gen Zhang <gz218@pc31.tcm.phy.cam.ac.uk>
2009-10-29 17:04:27 +0000

    forgot to scale up the resolution

--- ABC/infer_oes_eyp.m
+++ ABC/infer_oes_eyp.m
@@ -3,9 +3,9 @@ function infer_oes_eyp
 t1 = 3.0+1/7; % 8 days
 data1 = [2 0 16; 1 1 18; 0 2 20; 3 0 4; 2 1 19; 1 2 10; 0 3 7; 4 0 3; 3 1 8; 2 2 5; 1 3 1; 0 4 4; 4 1 5; 3 2 10; 2 3 3; 1 4 1; 0 5 1; 6 0 1; 5 1 2; 4 2 4; 3 3 3; 2 4 1; 1 5 2; 6 1 1; 5 2 2; 3 4 1; 6 2 3; 5 3 1; 4 4 4; 6 3 1; 5 4 1; 4 5 2; 6 4 1; 9 2 1; 8 3 2; 10 3 1; 9 4 1; 8 5 1; 10 4 1; 11 4 1; 12 4 1; 16 6 1; 18 5 1];
 
-rhos = linspace(0.1, 0.8, 10);
-rs = linspace(0.0, 0.5, 10);
-lambdas = linspace(0.5, 1.0, 10); % around 0.11*7/week
+rhos = linspace(0.1, 0.8, 30);
+rs = linspace(0.0, 0.5, 30);
+lambdas = linspace(0.5, 1.0, 20); % around 0.11*7/week
 
 infer_eyp(rhos, rs, lambdas, [t1], {data1}, 'infer_oes_eyp_bs');
 
