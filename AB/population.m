a452ead8ab115a6894139f5e76c6d14b9f1eea73
Gen Zhang <gz218@pc31.tcm.phy.cam.ac.uk>
2009-11-13 10:46:40 +0000

    cap probabilities

--- AB/population.m
+++ AB/population.m
@@ -7,6 +7,7 @@ P = expv(t, lambda*(Tl + r * Tr) + gamma*Tg, P1, 1e-10);
 if sum(P) < 0.99 
     warning('losing probability: %f; lambda t = %f, gamma t = %f', 1 - sum(P), lambda*t, gamma*t);
 end
+P = max(P, 0); % no negative probabilities
 P = reshape(P, sz);
 
 end
