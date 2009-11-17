b30be163ff5d84dd8e976b3ef7d47511a5944886
Gen Zhang <genneth@buck.(none)>
2009-11-17 10:09:14 +0000

    switch to generating function

--- AB/population.m
+++ AB/population.m
@@ -1,12 +1,21 @@
 function P = population(Tl, Tr, Tg, lambda, r, gamma, t, P0)
 
-% dP/dt = T P ==> P = P0 * exp(T t)
-sz = size(P0);
-P1 = reshape(P0, prod(sz), 1);
-P = expv(t, lambda*(Tl + r * Tr) + gamma*Tg, P1, 1e-10);
-if sum(P) < 0.99 
-    warning('losing probability: %f; lambda t = %f, gamma t = %f', 1 - sum(P), lambda*t, gamma*t);
+if 0==1
+
+    % dP/dt = T P ==> P = P0 * exp(T t)
+    sz = size(P0);
+    P1 = reshape(P0, prod(sz), 1);
+    P = expv(t, lambda*(Tl + r * Tr) + gamma*Tg, P1, 1e-10);
+    if sum(P) < 0.99 
+        warning('losing probability: %f; lambda t = %f, gamma t = %f', 1 - sum(P), lambda*t, gamma*t);
+    end
+    P = reshape(P, sz);
+
+else
+    
+    N = size(P0, 1) + 1;
+    P = exact_pops(lambda, r, gamma, t, N);
+
 end
-P = reshape(P, sz);
 
 end
