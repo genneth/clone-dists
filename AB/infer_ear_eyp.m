1c6d9f29841e020d375105915c7280a50811e0e3
Gen Zhang <gz218@pc31.tcm.phy.cam.ac.uk>
2009-12-23 12:13:09 +0000

    full run of ear inference

--- AB/infer_ear_eyp.m
+++ AB/infer_ear_eyp.m
@@ -16,6 +16,10 @@ data3 = [2 57; 3 38; 4 20; 5 24; 6 15; 7 8; 8 2; 9 8; 10 6; 11 2; 12 2; 13 5; 16
 t4 = 52;
 data4 = [2 33; 3 26; 4 21; 5 15; 6 10; 7 9; 8 6; 9 8; 10 6; 11 1; 12 5; 13 6; 14 2; 15 3; 16 7; 17 2; 19 1; 20 1; 21 1; 25 2; 35 1; 44 1; 50 1];
 
-infer_eyp(linspace(0.1, 0.8, 31), linspace(0.0, 0.5, 30), linspace(1/6, 1/3, 29), [t1 t2 t3 t4], {data1 data2 data3 data4}, 'ear-eyp-b');
+rhos = linspace(0.1, 0.8, 30);
+rs = linspace(0.0, 0.5, 31);
+lambdas = linspace(1/6, 1/2, 32);
+%infer_eyp(rhos, rs, 0.25, [t1 t2 t3 t4], {data1 data2 data3 data4}, 'ear-eyp-b-fixed-lambda');
+infer_eyp(rhos, rs, lambdas, [t4 t1 t3 t2], {data4 data1 data3 data2}, 'ear-eyp-b');
 
 end
