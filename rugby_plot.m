bcf348900e5176aaf557c21581fbe9b5a5f1f7b2
Gen Zhang <gz218@pc31.tcm.phy.cam.ac.uk>
2010-01-09 11:02:03 +0000

    quick plot of ABC inference

---
+++ rugby_plot.m
@@ -0,0 +1,9 @@
+function rugby_plot(gf, file)
+
+load(file);
+
+[rho1,rho2, r1,r2, lambda1,lambda2] = extract_moments(file);
+
+contourslice(gf, rhos,rs,lambdas, max(0,pxd), rho1,r1,lambda1);
+
+end
