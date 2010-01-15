aa8c00b4f6581c6acf17314887cbad257fe3ab53
Gen Zhang <gz218@pc31.tcm.phy.cam.ac.uk>
2010-01-15 11:32:01 +0000

    label axes

--- rugby_plot.m
+++ rugby_plot.m
@@ -5,5 +5,8 @@ load(file);
 [rho1,rho2, r1,r2, lambda1,lambda2] = extract_moments(file);
 
 contourslice(gf, rhos,rs,lambdas, max(0,pxd), rho1,r1,lambda1);
+xlabel('$\rho$', 'Interpreter', 'latex');
+ylabel('$r$', 'Interpreter', 'latex');
+zlabel('$\lambda$', 'Interpreter', 'latex');
 
 end
