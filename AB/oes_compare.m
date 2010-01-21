fc9e04d9f418d4fd73767b9d7e86c42a8a88a4bf
Gen Zhang <genneth@buck.(none)>
2010-01-21 15:56:08 +0000

    fix average theoretical average

--- AB/oes_compare.m
+++ AB/oes_compare.m
@@ -34,7 +34,7 @@ gamma = lambda * rho / (1-rho);
 tau = rho / (r * lambda);
 
 [p0, ts2] = xi(lambda, r, rho, max(ts), 0);
-loglog(newplot(figure), ts, av, '*', ts2, (1 + 1/rho * (1-exp(-gamma*ts2))) ./ (1 - p0), '-');
+loglog(newplot(figure), ts, av, '*', ts2, (1 + (lambda/gamma)*(1-exp(-gamma*ts2))) ./ (1 - p0), '-');
 set(gca, 'XLim', [0.1 100]);
 
 gh = newplot(figure);
