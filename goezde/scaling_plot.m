832266598afb56a6d7b1d26cf5bde29651523726
Gen Zhang <genneth@gmail.com>
2010-08-10 14:14:04 +0100

    cumulative plot, to make easier comparisons with experiment

--- goezde/scaling_plot.m
+++ goezde/scaling_plot.m
@@ -5,20 +5,22 @@ gh = newplot(fh);
 set(gh, 'NextPlot', 'add');
 %set(gh, 'YScale', 'log');
 set(gh, 'XLim', [0 5]);
+drawnow;
 
 ps = clone_dist(r1, r2, ts, n);
 
 for i = 1:numel(ts)
     av = average_size(r1, r2, ts(i));
     p = ps(:,i);
-    semilogy(gh, (0:(n-1)) ./ av, p .* av);
+    c = 1 - cumsum(p);
+    plot(gh, (1:n) ./ av, c);
     drawnow;
 end
 
 xlabel(gh, 'scaled size', 'FontName', 'Times', 'FontSize', 8);
-
+ylabel(gh, 'cumulative distribution', 'FontName', 'Times', 'FontSize', 8);
 set(gh, 'FontName', 'Times', 'FontSize', 8);
 
-print(fh, '-dpng', '-painters', '-r300', 'scaling_plot');
+print(fh, '-dpng', 'scaling_plot');
 
 end
