b5d421b3e8844b289aa394d747c6f6a82634d340
Gen Zhang <genneth@gmail.com>
2010-08-25 10:13:03 +0100

    new ear data

--- ABC/infer_ear_eyp.m
+++ ABC/infer_ear_eyp.m
@@ -1,15 +1,23 @@
 function infer_ear_eyp
 
-t1 = 3.0;
-data1 = [2 0 11; 1 1 4; 0 2 6; 3 0 1; 2 1 3; 0 3 1; 4 0 1; 4 1 1; 6 1 1];
+t = 6.0;
+databs = [
+    0	133	33	5	3;
+    40	46	7	2	0;
+    42	38	12	0	0;
+    12	20	3	1	1;
+    7	7	4	2	0;
+    0	2	2	0	0;
+    1	0	0	0	0
+    ];
+databs(0+1, :)   = 0;
+databs(1+1, 0+1) = 0;
+data = Pbs2lst(databs);
 
-t2 = 6.0;
-data2 = [2 0 42; 3 0 12; 4 0 7; 6 0 1; 1 1 46; 2 1 38; 3 1 20; 4 1 7; 5 1 2; 0 2 33; 1 2 7; 2 2 12; 3 2 3; 4 2 4; 5 2 2; 0 3 5; 1 3 2; 3 3 1; 4 3 2; 0 4 3; 3 4 1];
+rhos = linspace(0.3, 0.7, 30);
+rs = linspace(0.01, 0.35, 31);
+lambdas = linspace(0.2, 0.4, 29);
 
-rhos = linspace(0.5, 0.7, 30);
-rs = linspace(0.15, 0.35, 31);
-lambdas = linspace(0.2, 0.3, 29);
-
-infer_eyp(rhos, rs, lambdas, [t1 t2], {data1 data2}, 'ear_eyp_bs');
+infer_eyp3(rhos, rs, lambdas, [t], {data}, 'ear_eyp_bs_cond');
 
 end
