371f50054043bf986f93d8c5c15a753a13b6a5f1
Gen Zhang <genneth@gmail.com>
2011-06-06 16:00:45 +0100

    check tau with suprabasal data

---
+++ hutch/oesophagus/basal_check.m
@@ -0,0 +1,22 @@
+function basal_check
+
+oesophagus_data;
+
+% use the suprabasal counts to estimate tau
+
+basal = {};
+for i=1:numel(ts3)
+    basal{i} = normal{i}(0+1,:);
+    basal{i}(1+1) = 0; % remove singles
+end
+
+stream = RandStream('mt19937ar','seed',sum(100*clock));
+RandStream.setDefaultStream(stream);
+sample2(...
+    @()(random('beta', 2, 2) / 2), ...
+    @()(random('logn', log(1), log(2))), ...
+    @()(random('logn', 0, log(2))), ...
+    ts3, basal, 100, 100, ...
+    'basal.mat');
+
+end
