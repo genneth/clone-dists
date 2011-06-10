213f9a29b2f3bda2fe38c9c114b06692f3210899
Gen Zhang <genneth@gmail.com>
2011-06-10 14:08:42 +0100

    average logs for the rates and give gamma and mu

--- posterior_outputs.m
+++ posterior_outputs.m
@@ -5,6 +5,7 @@ rf = @(r,g,l) r;
 rhof = @(r,g,l) g/(1+g);
 lf = @(r,g,l) log(l);
 tf = @(r,g,l) log(g/(1+g)/(r*l));
+gf = @(r,g,l) g;
 sq = @(f) (@(r,g,l) f(r,g,l)^2);
 
 Z = linear_quadrature_integrate3(constf, samples, pf);
@@ -12,17 +13,21 @@ r1 = linear_quadrature_integrate3(rf, samples, pf) / Z;
 rho1 = linear_quadrature_integrate3(rhof, samples, pf) / Z;
 ll1 = linear_quadrature_integrate3(lf, samples, pf) / Z;
 lt1 = linear_quadrature_integrate3(tf, samples, pf) / Z;
+g1 = linear_quadrature_integrate3(gf, samples, pf) / Z;
 
-fprintf('r = %.3f, ρ = %.3f, 7/λ = %.2f days, 7*τ = %.2f days\n', r1, rho1, 7/exp(ll1), 7*exp(lt1));
+% fprintf('r = %.3f, ρ = %.3f, 7/λ = %.2f days, 7*τ = %.2f days\n', r1, rho1, 7/exp(ll1), 7*exp(lt1));
 
 r2 = linear_quadrature_integrate3(sq(rf), samples, pf) / Z;
 rho2 = linear_quadrature_integrate3(sq(rhof), samples, pf) / Z;
 ll2 = linear_quadrature_integrate3(sq(lf), samples, pf) / Z;
 lt2 = linear_quadrature_integrate3(sq(tf), samples, pf) / Z;
+g2 = linear_quadrature_integrate3(sq(gf), samples, pf) / Z;
 
 fprintf('r = %.3f ± %.3f\n', r1, sqrt(r2 - r1^2));
 fprintf('ρ = %.3f ± %.3f\n', rho1, sqrt(rho2 - rho1^2));
 fprintf('λ = %.2f / weeks + %.0f%% - %.0f%%\n', exp(ll1), (exp(sqrt(ll2 - ll1^2)) - 1)*100, (1 - exp(-sqrt(ll2 - ll1^2)))*100);
 fprintf('τ = %.2f weeks + %.0f%% - %.0f%%\n', exp(lt1), (exp(sqrt(lt2 - lt1^2)) - 1)*100, (1 - exp(-sqrt(lt2 - lt1^2)))*100);
+fprintf('γ = %.3f ± %.3f\n', g1, sqrt(g2-g1^2));
+fprintf('μ = %.3f ± %.3f\n', rho1/0.82, sqrt(rho2 - rho1^2) / 0.82);
 
 end
